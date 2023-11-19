23020fde3cc44607bbd0788ed9f3c976391332b6
JAYDEN\12633 <1263346845@qq.com>
2023-11-19 13:23:08 +0800

    项目骨架搭建完成

---
+++ mall-order/src/test/java/com/jayden/mallorder/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayden.mallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
