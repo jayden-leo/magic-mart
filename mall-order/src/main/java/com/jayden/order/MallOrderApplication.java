989c1ee1c0d67fd42ab690cf9cc31868bc94f71c
JAYDEN\12633 <1263346845@qq.com>
2023-11-20 01:50:45 +0800

    nacos注册

--- mall-order/src/main/java/com/jayden/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/jayden/order/MallOrderApplication.java
@@ -3,9 +3,11 @@ package com.jayden.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.jayden.order.dao")
+@EnableDiscoveryClient
 public class MallOrderApplication {
 
     public static void main(String[] args) {
